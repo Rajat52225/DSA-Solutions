b6475ddd7c9d3e06d3650282af24f3e5d232ceed
Rajat Sharma <rajatsre455@gmail.com>
2026-07-19 20:39:28 +0530

    Add rank transform function for an array
    Implemented the rank transform of an array using sorting and a map to store ranks.

---
+++ Array/Rank.cpp
@@ -0,0 +1,31 @@
+/*
+Leet Code - 1331 :- Rank transform of an array.
+So approach was like:-
+1. Copy the array to other vector and sort in increasing order.
+2. Then just rank 1 to till there are unqiue elements and store number and rank in map.
+3. Then just traverse the original array and replace number with rank from map.
+
+Time Complexity : O(n).
+Space Complexity : O(n*n) .
+
+*/
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+       vector<int>v=arr;
+       unordered_map<int,int>mp;
+       sort(v.begin(),v.end());
+       int rank=1;
+       for(int i=0;i<v.size();i++){
+        if(mp.find(v[i])==mp.end()){
+        mp[v[i]]=rank;
+        rank++;
+        }
+       }
+       for(int i=0;i<arr.size();i++){
+        arr[i]=mp[arr[i]];
+       }
+       return arr;
+    }
+};
